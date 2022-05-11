649f43aced4bb101973724431370fa497d58848c
oppen <arieloppenheimer@outlook.cl>
2022-05-10 21:23:15 -0400

    destroy

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-    has_many :concerts
+    has_many :concerts, dependent: :destroy
     enum group_type: [:boys, :girls, :band]
     def q_concert
         self.concerts.count
